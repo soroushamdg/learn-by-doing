16449cb84e4181d91860cf5e28bb94f4335c30e3
soroushamdg <soroushmpour@gmail.com>
2022-10-15 14:18:00 -0400

    Card View design

---
+++ Learn By Doing/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Learn By Doing
+//
+//  Created by Soro on 2022-10-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CardView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
