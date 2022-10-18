737947c11ace10f15375ad213afe5e3f55495ea1
soroushamdg <soroushmpour@gmail.com>
2022-10-17 21:23:22 -0400

    Card View in scroll view in hstack

--- Learn By Doing/ContentView.swift
+++ Learn By Doing/ContentView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        CardView()
+        ScrollView(.horizontal,showsIndicators: false){
+            HStack(alignment: .center,spacing: 20){
+                ForEach(0..<6) { item in
+                    CardView()
+                }
+            }
+            .padding(20)
+        }
     }
 }
 
